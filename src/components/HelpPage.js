c48c8e2ab0f3aaa4814c760689313623c06824b3
enricofossanai <enrico@ossanai.com>
2020-05-15 13:12:06 -0300

    Class 126

--- src/components/HelpPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpPage = () => (
-    <div>
-      This is Help
-    </div>
-  )
-
-export default HelpPage;
--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,18 +3,22 @@ import {shallow} from 'enzyme'
 import {AddExpensePage} from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
+let addExpense, history, wrapper;
+
+beforeEach(() => {
+  addExpense = jest.fn();
+  history = {
+    push: jest.fn()
+  }
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history}/>)
+})
+
 test('should render AddExpensePage', () => {
-  const onSubmit = jest.fn();
-  const history = {push: jest.fn()}
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
   expect(wrapper).toMatchSnapshot()
 })
 
 test('should handle onSubmit', () => {
-  const onSubmit = jest.fn()
-  const history = {push: jest.fn()}
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>)
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenCalledWith('/')
-  expect(onSubmit).toHaveBeenCalledWith(expenses[1])
+  expect(addExpense).toHaveBeenCalledWith(expenses[1])
 })
