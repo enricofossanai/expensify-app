7be58937b49fc0fc607ab5a0cc8b9a980ce53559
enricofossanai <enrico@ossanai.com>
2020-05-19 13:36:08 -0300

    class 140

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary';
 
 export const ExpenseDashboardPage = () => (
     <div>
       <ExpenseListFilters/>
+      <ExpensesSummary/>
       <ExpenseList/>
     </div>
   )
